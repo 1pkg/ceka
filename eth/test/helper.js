f44c204d92846199c6837824e11d547436f8e0ef
Kostiantyn Masliuk <1pkg@protonmail.com>
2019-04-22 17:17:15 +0300

    update test set structure. add additional helper for all test related staff. add first fobll tests fix related cotract part.

---
+++ eth/test/helper.js
@@ -0,0 +1,63 @@
+const crypto = require('crypto')
+const ethUtils = require('ethereumjs-util')
+
+const SZABO = 1e12
+const FINNEY = 1e15
+const ETHER = 1e18
+
+const MINUTES = 60
+const HOURS = 60 * MINUTES
+
+const timeGap = async time => {
+    await new Promise((resolve, reject) => {
+        web3.currentProvider.send(
+            {
+                jsonrpc: '2.0',
+                method: 'evm_increaseTime',
+                params: [time],
+                id: new Date().getTime(),
+            },
+            (err, result) => {
+                if (err) {
+                    return reject(err)
+                }
+                return resolve(result)
+            },
+        )
+    })
+    await new Promise((resolve, reject) => {
+        web3.currentProvider.send(
+            {
+                jsonrpc: '2.0',
+                method: 'evm_mine',
+                id: new Date().getTime(),
+            },
+            (err, result) => {
+                if (err) {
+                    return reject(err)
+                }
+                const newBlockHash = web3.eth.getBlock('latest').hash
+                return resolve(newBlockHash)
+            },
+        )
+    })
+    return Promise.resolve(web3.eth.getBlock('latest'))
+}
+
+const rndAddr = () => {
+    return (
+        '0x' + ethUtils.privateToAddress(crypto.randomBytes(32)).toString('hex')
+    )
+}
+
+module.exports = {
+    SZABO,
+    FINNEY,
+    ETHER,
+
+    MINUTES,
+    HOURS,
+
+    timeGap,
+    rndAddr,
+}
