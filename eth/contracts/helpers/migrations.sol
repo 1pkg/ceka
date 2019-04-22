64045fa567f9a59c29e5541a7c90a447c8823d26
John Brown <alfa0dude@gmail.com>
2019-04-22 23:19:21 +0300

    add more fobll tests.

--- eth/contracts/helpers/migrations.sol
+++ eth/contracts/helpers/migrations.sol
@@ -4,14 +4,17 @@ import './ownable.sol';
 
 /// @title used only for truffle
 contract Migrations is Ownable {
-  uint public last_completed_migration;
+    /// @dev truffle specific member
+    uint public last_completed_migration;
 
-  function setCompleted(uint completed) public owner {
-    last_completed_migration = completed;
-  }
+    /// @dev truffle specific member
+    function setCompleted(uint completed) public owner {
+        last_completed_migration = completed;
+    }
 
-  function upgrade(address new_address) public owner {
-    Migrations upgraded = Migrations(new_address);
-    upgraded.setCompleted(last_completed_migration);
-  }
+    /// @dev truffle specific member
+    function upgrade(address new_address) public owner {
+        Migrations upgraded = Migrations(new_address);
+        upgraded.setCompleted(last_completed_migration);
+    }
 }
